548131edccd1bf92f5ef088f9a6b37e28f37d351
marcoslima12 <marcosantonio-10@live.com>
2024-08-28 20:29:48 -0300

    basic home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../contexts/AuthContext';
-import { logOut } from '../../services/auth';
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
+import { logOut } from "../../services/auth";
+import { useEffect } from "react";
 
 export const Home: React.FC = () => {
   const { currentUser } = useAuth();
@@ -9,16 +10,24 @@ export const Home: React.FC = () => {
   const handleLogout = async () => {
     try {
       await logOut();
-      navigate('/login'); // Redireciona para a página de login após logout
+      navigate("/login"); // Redireciona para a página de login após logout
     } catch (err) {
-      console.error('Logout failed', err);
+      console.error("Logout failed", err);
     }
   };
 
+  useEffect(() => {
+    if (!currentUser) {
+      navigate("/login"); // Se não houver usuário autenticado, redireciona para a página de login
+    }
+  }, [currentUser, navigate]);
+
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-lg bg-white p-8 rounded-lg shadow-lg">
-        <h1 className="text-3xl font-bold text-center mb-6">Welcome, {currentUser?.displayName}</h1>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="w-full max-w-lg  p-8 rounded-lg shadow-lg">
+        <h1 className="text-3xl font-bold text-center mb-6">
+          Welcome, {currentUser?.displayName}
+        </h1>
         <button
           onClick={handleLogout}
           className="w-full bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
