6665881b235bca5ee1d5c8d9fb4e30477bab0fc6
marcoslima12 <marcosantonio-10@live.com>
2024-08-27 21:57:38 -0300

    create basic theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <h1 className="text-3xl text-blue-500 font-bold underline">
-      Hello world!
-    </h1>
+    <div className="w-full h-screen bg-primary text-white">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1E1E26',
+        secondary: '#1A1A21',
+        terciary: '#16161C',
+        quaternary: '#1E1E26',
+        grey: '#2D2D3B',
+        white: '#F9F9F9',
+        highlight: '#EE69AC'
+      },
+      fontFamily: {
+        inter: ['Inter', "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
