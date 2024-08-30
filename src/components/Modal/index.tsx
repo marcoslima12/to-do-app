3fb455af9cc43953b56e640ed82ad4ed37870111
marcoslima12 <marcosantonio-10@live.com>
2024-08-30 16:56:09 -0300

    modal aaccessibility

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -11,7 +11,7 @@ const schema = z.object({
   desc: z.string().optional(),
   deadline: z.date({
     required_error: "Deadline é obrigatório",
-    invalid_type_error: "Data inválida"
+    invalid_type_error: "Data inválida",
   }),
 });
 
@@ -32,73 +32,107 @@ export const TaskModal: React.FC<TaskModalProps> = ({
     register,
     handleSubmit,
     formState: { errors },
-    reset, 
+    reset,
   } = useForm<TaskFormData>({
     resolver: zodResolver(schema),
   });
 
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    const dialog = dialogRef.current;
+
+    if (isOpen && dialog) {
+      dialog.showModal();
+    } else if (dialog) {
+      dialog.close();
+    }
+
+    const handleEscape = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+
+    dialog?.addEventListener("keydown", handleEscape);
+
+    return () => {
+      dialog?.removeEventListener("keydown", handleEscape);
+    };
+  }, [isOpen, onClose]);
+
   const handleFormSubmit = (data: TaskFormData) => {
     onSubmit(data);
-    reset(); 
+    reset();
+  };
+
+  const handleClickOutside = () => {
+    if (dialogRef.current) {
+      
+      onClose();
+    }
   };
 
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-primary bg-opacity-50 flex justify-center items-center">
-      <div className="bg-terciary p-6 rounded-lg shadow-lg w-11/12 md:w-3/5 max-w-lg relative">
-        <button
-          className="absolute top-4 right-4"
-          onClick={onClose}
-          aria-label="Close"
-        >
-          <Close />
-        </button>
-        <h2 className="text-xl mb-4 text-highlight">Criar tarefa</h2>
+    <dialog ref={dialogRef}
+    onClick={handleClickOutside}>
+      <div className="fixed inset-0 bg-primary backdrop-blur-sm bg-opacity-50 flex justify-center items-center">
+        <div onClick={(e) => e.stopPropagation()} className="bg-terciary p-6 rounded-lg shadow-lg w-11/12 md:w-3/5 max-w-lg relative">
+          <button
+            className="absolute top-4 right-4"
+            onClick={onClose}
+            aria-label="Close"
+          >
+            <Close />
+          </button>
+          <h2 className="text-xl mb-4 text-highlight">Criar tarefa</h2>
 
-        <form onSubmit={handleSubmit(handleFormSubmit)}>
-          <div className="mb-4">
-            <Input
-              id="title"
-              label="Título"
-              type="text"
-              error={errors.title}
-              register={register("title")}
-              placeholder="Título"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <Input
-              id="desc"
-              label="Descrição"
-              type="text"
-              error={errors.desc}
-              register={register("desc")}
-              placeholder="Descrição"
-            />
-          </div>
-          <div className="mb-4 ">
-            <Input
-              id="date"
-              label="Deadline"
-              type="date"
-              error={errors.deadline}
-              register={{
-                ...register("deadline", {
-                  valueAsDate: true,
-                }),
-              }}
-              placeholder="Data"
-              required
-            />
-          </div>
-          <div className="flex gap-4">
-            <Button text="Cancelar" type="button" onClick={onClose} />
-            <Button text="Adicionar" type="submit" />
-          </div>
-        </form>
+          <form onSubmit={handleSubmit(handleFormSubmit)}>
+            <div className="mb-4">
+              <Input
+                id="title"
+                label="Título"
+                type="text"
+                error={errors.title}
+                register={register("title")}
+                placeholder="Título"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <Input
+                id="desc"
+                label="Descrição"
+                type="text"
+                error={errors.desc}
+                register={register("desc")}
+                placeholder="Descrição"
+              />
+            </div>
+            <div className="mb-4 ">
+              <Input
+                id="date"
+                label="Deadline"
+                type="date"
+                error={errors.deadline}
+                register={{
+                  ...register("deadline", {
+                    valueAsDate: true,
+                  }),
+                }}
+                placeholder="Data"
+                required
+              />
+            </div>
+            <div className="flex gap-4">
+              <Button text="Cancelar" type="button" onClick={onClose} />
+              <Button text="Adicionar" type="submit" />
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </dialog>
   );
 };
